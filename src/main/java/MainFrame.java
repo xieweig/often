71171128c1f08ffa62bca60011392f7d5ece0cf9
xieweiguang <3162217932@qq.com>
2018-05-31 14:28:18 +0800

    Initial commit add all commit all

---
+++ src/main/java/MainFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainFrame {
+
+    public  static  void main(String [] args){
+       new MyFrame("ok");
+    }
+}
---
+++ src/main/java/MyFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame(String title) throws HeadlessException {
+        super(title);
+
+        this.setSize(500, 300);
+        this.setTitle("====niit====exam====");
+        this.setLayout(new FlowLayout());
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        this.add(new JButton("ok1"));
+        this.add(new JButton("ok2"));
+        this.add(new JButton("ok31"));
+        JScrollPane jScrollPane = new JScrollPane(new TextArea());
+        this.add(jScrollPane);
+
+        this.setVisible(true);
+    }
+}
